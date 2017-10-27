1de963cd663177664f7a88e662daa062fb92ce04
Denson <ydswhut@163.com>
2017-10-27 11:11:59 +0800

    LC3

---
+++ src/LC3.java
@@ -0,0 +1,55 @@
+/*
+ * 最大无重复子串
+ */
+public class LC3 {
+    public static void main(String[] args) {
+        String s = "abba";
+        System.out.println(new Solution().lengthOfLongestSubstring(s));
+    }
+}
+
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] pos = new int[256];
+        int start = 0;
+        int max = 0;
+        for (int i = 0; i != s.length(); i++) {
+            if (pos[s.charAt(i)] > start) {
+                start = pos[s.charAt(i)];
+            }
+            pos[s.charAt(i)] = i + 1;
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+}
+
+
+/*version 1
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length()==0) return 0;
+        for (int len = s.length(); len > 1; len--) {
+            for(int i = 0;i+len-1<s.length();i++){
+                if(noRepeating(s.substring(i,i+len))) {
+                    return len;
+                }
+            }
+        }
+        return 1;
+    }
+
+    public boolean noRepeating(String s) {
+        if (s.length() == 1)
+            return true;
+        for (int i = 1; i < s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (s.charAt(i) == s.charAt(j))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
+*/
---
+++ src/Niuke1.java
@@ -0,0 +1,46 @@
+
+/*
+ * 在一个二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。
+ * 请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+ */
+
+
+//Wrong Answer!!!!
+public class Niuke1 {
+
+    public static void main(String[] args) {
+        int[][] a = {{1,2,8,9}, {2, 4, 9,12}, {4,7,10,13},{6,8,11,15}};
+        int target = 15;
+        System.out.println(new Niuke1().Find(target, a));
+
+    }
+
+    boolean Find(int target, int[][] array) {
+        int row = array.length;
+        int col = array[0].length;
+
+        int left = 0, right = col - 1, mid = right / 2;
+        int mup = array[0][mid];
+        int mdn = array[row - 1][mid];
+
+        while (target < mup || target > mdn) {
+            if (right == left) return false;
+            if (left == right - 1) {
+                if(target > array[row - 1][left] && target < array[0][right]) return false;
+                if(mid == left) mid=mid+1;
+            }
+            if (target < mup) right = mid;
+            else left = mid;
+
+            mid = (left + right) / 2;
+            mup = array[0][mid];
+            mdn = array[row - 1][mid];
+        }
+        for (int i = 0; i < row; i++) {
+            if (array[i][mid] == target)
+                return true;
+        }
+        return false;
+    }
+
+}
