726e9cdfa9c587587e3919128381ccc65fdc8ec6
Denson <ydswhut@163.com>
2017-11-01 11:35:09 +0800

    LC7

---
+++ src/LC7.java
@@ -0,0 +1,57 @@
+/**
+ * 整数逆序
+ */
+public class LC7 {
+    public static void main(String[] args) {
+        System.out.println(new LC7().reverse(122332222));
+    }
+
+    public int reverse(int x) {
+        int abs = x;
+        if (x < 0) abs = -x;
+        String s = "";
+        while (abs != 0) {
+            s += abs % 10;
+            abs = abs / 10;
+        }
+        int result;
+        try {
+            if (x < 0) result = Integer.parseInt(s) * -1;
+            else result = Integer.parseInt(s);
+        }catch (Exception e){
+            result = 0;
+        }
+        return result;
+    }
+
+
+//Better Answer 1：利用long过渡
+//    public int reverse(int x) {
+//        long rev= 0;
+//        while( x != 0){
+//            rev= rev*10 + x % 10;
+//            x= x/10;
+//            if( rev > Integer.MAX_VALUE || rev < Integer.MIN_VALUE)
+//                return 0;
+//        }
+//        return (int) rev;
+//    }
+
+
+//Better Answer 2
+//    public int reverse(int x)
+//    {
+//        int result = 0;
+//        while (x != 0)
+//        {
+//            int tail = x % 10;
+//            int newResult = result * 10 + tail;
+//            if ((newResult - tail) / 10 != result)
+//                return 0;
+//            result = newResult;
+//            x = x / 10;
+//        }
+//
+//        return result;
+//    }
+}
