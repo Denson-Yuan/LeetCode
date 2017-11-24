28cf0cbe12e158aa56243205d373e78fc2aef2a8
Denson <ydswhut@163.com>
2017-11-24 16:53:51 +0800

    LC27 & LC28

---
+++ src/LC27.java
@@ -0,0 +1,28 @@
+/**
+ * Remove Element
+ * 从数组中移除元素
+ */
+public class LC27 {
+    public static void main(String[] args) {
+        int[] nums = {3};
+        System.out.println(new LC27().removeElement(nums, 3));
+        for (int i : nums) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public int removeElement(int[] nums, int val) {
+        if (nums.length == 0) return 0;
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            while (i <= j && nums[i] != val) i++;
+            while (j >= i && nums[j] == val) j--;
+            if (i < j) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+            }
+        }
+        return i;
+    }
+}
---
+++ src/LC28.java
@@ -0,0 +1,27 @@
+/**
+ * Implement strStr()
+ * 寻找子串位置
+ */
+public class LC28 {
+    public static void main(String[] args) {
+        String haystack = "misspi", needle = "pi";
+        System.out.println(new LC28().strStr(haystack, needle));
+    }
+
+    public int strStr(String haystack, String needle) {
+        // if (needle.length() == 0) return 0;
+        // if (haystack.equals(needle)) return 0;
+        // <= , 不是 < !!! 则不用考虑上述情况
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            boolean flag = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) return i;
+        }
+        return -1;
+    }
+}
